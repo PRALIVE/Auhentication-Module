f63b8bf1bb600e11d3ef4ed1212072758c63360d
Pankaj <richyadav6@gmail.com>
2022-06-02 11:28:57 +0530

    gitignore commit

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 //jshint esversion:6
-require('dotenv').config();
+//require('dotenv').config();
 const express=require("express");
 const bodyParser=require("body-parser");
 const ejs=require("ejs");
 const mongoose=require("mongoose");
 const encrypt=require("mongoose-encryption");
+const md5=require("md5");
 
 const app=express();
 app.set('view engine','ejs');
@@ -18,9 +19,8 @@ const userSchema=new mongoose.Schema({
   password:String
 });
 
-
-const secret=process.env.SECRET;
-userSchema.plugin(encrypt,{secret:secret,encryptedFields:["password"]});
+//const secret=process.env.SECRET;
+//userSchema.plugin(encrypt,{secret:secret,encryptedFields:["password"]});
 
 const User=mongoose.model("User",userSchema);
 
@@ -38,7 +38,7 @@ app.get("/register",function(req,res){
 
 app.post("/login",function(req,res){
   const username=req.body.username;
-  const password=req.body.password;
+  const password=md5(req.body.password);
 
   User.findOne({email:username},function(err,foundUser){
     if(err){
@@ -57,7 +57,7 @@ app.post("/register",function(req,res){
 
   const newUser=new User({
     email:req.body.username,
-    password:req.body.password
+    password:md5(req.body.password)
   });
 
   newUser.save(function(err){
